d3cc450c00414588924ffeed296b104ce9c54dcc
atipikrh <atipikrh@users.noreply.github.com>
2026-05-19 12:32:39 +0200

    chore: supprimer les scripts .bat temporaires commites par accident

--- set_env.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\cnoah\Documents\GitHub\smart-fleet-africa
-vercel env rm VITE_AUTH_PROVIDER production --scope atipik --yes
-set /p DUMMY=clerk<nul | vercel env add VITE_AUTH_PROVIDER production --scope atipik
-echo DONE
-del set_env.bat
