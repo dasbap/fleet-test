62ba1ab8a70dca2f678ae1d3b3c65f427d5a42d7
Da Silva Baptiste <144991567+dasbap@users.noreply.github.com>
2026-09-08 09:17:09 +0000

    feat: register local super-admin delete routes

--- src/server/http/app.ts
+++ src/server/http/app.ts
@@ -17,6 +17,7 @@ import { registerHealthRoutes } from "./routes/health.js";
 import { registerAdminDemoRoutes } from "./routes/adminDemo.js";
 import { registerAdminProspectSecurityRoutes } from "./routes/adminProspectSecurity.js";
 import { registerAdminUserSecurityRoutes } from "./routes/adminUserSecurity.js";
+import { registerAdminDestructiveSecurityRoutes } from "./routes/adminDestructiveSecurity.js";
 import { registerPasswordChangeRoutes } from "./routes/passwordChange.js";
 import { registerDemoRequestRoutes } from "./routes/demoRequest.js";
 import {
@@ -112,6 +113,7 @@ export function createServerApp() {
   registerPasswordChangeRoutes(app);
   registerAdminProspectSecurityRoutes(app);
   registerAdminUserSecurityRoutes(app);
+  registerAdminDestructiveSecurityRoutes(app);
   registerAdminDemoRoutes(app);
   registerDemoRequestRoutes(app);
   registerGpsIngestRoutes(app);
